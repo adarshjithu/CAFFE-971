c03f67f20b1ea7ced7732e1e44db95d2e036d6bd
sahla <sahlaanasks@gmail.com>
2025-04-03 19:29:04 +0530

    edited header layouts

--- client/src/pages/User/AboutUs.tsx
+++ client/src/pages/User/AboutUs.tsx
@@ -1,11 +1,9 @@
 
-import Header from '../../components/user/Header/Header'
 import About from '../../components/user/AboutUs/About'
 
 function AboutUs() {
   return (
     <div>
-        <Header/>
         <About/>
     </div>
   )
