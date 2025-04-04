99ecd8e7a5699ba42bb20cb9217d335fb4db6f0d
adarsh <adarshjithu10@gmail.com>
2025-04-04 12:39:13 +0530

    Updated CategoryModel

--- server/src/models/categoryModel.ts
+++ server/src/models/categoryModel.ts
@@ -4,7 +4,7 @@ import { ICategory } from "../interface/Models/ICategory";
 
 const categorySchema = new mongoose.Schema<ICategory>({
     name:{type:String,required:true},
-    image:{type:String,required:true}
+    image:{type:String,required:true} 
 },{timestamps:true});
 
 const Category  = mongoose.model<ICategory>('Category',categorySchema);
