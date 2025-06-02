8079b92c638f66117b5c22509a1d61561fde6d87
adarsh <adarshjithu10@gmail.com>
2025-06-02 19:05:36 +0530

    Tables layout updated

--- client/src/components/user/Seating/TableCard.tsx
+++ client/src/components/user/Seating/TableCard.tsx
@@ -4,59 +4,79 @@ import { useDispatch, useSelector } from "react-redux";
 import { removeTables, setTables } from "../../../features/user/packageSelectionSlice";
 import { IRootState } from "../../../app/store";
 
-export const TableCard = ({ table ,buttonVisibility}: { table: ITable,buttonVisibility:boolean }) => {
-    const dispatch = useDispatch();
+export const TableCard = ({
+  table,
+  buttonVisibility,
+}: {
+  table: ITable;
+  buttonVisibility: boolean;
+}) => {
+  const dispatch = useDispatch();
+  const { tables } = useSelector((data: IRootState) => data?.packageSelectionData);
 
-    const { tables } = useSelector((data: IRootState) => data?.packageSelectionData);
+  const isSelected = tables?.some((obj: any) => obj?._id === table?._id);
 
-    return (
-        <div
-            className="bg-[#015D42]  text-white relative border border-white rounded-br-[85px] rounded-[15px] p-2 shadow-lg flex items-center"
-            style={{ borderWidth: "0.5px", borderColor: "rgba(255,255,255,0.3)" }}
-        >
-            <img src={table?.image} alt="most rated" className="w-34 h-34 rounded-lg mr-4 object-cover" />
+  return (
+    <div
+      className="bg-[#015D42] text-white relative border border-white rounded-br-[85px] rounded-[15px] p-3 shadow-lg flex flex-col sm:flex-row sm:items-center gap-4 sm:gap-6"
+      style={{ borderWidth: "0.5px", borderColor: "rgba(255,255,255,0.3)" }}
+    >
+      {/* Image */}
+      <img
+        src={table?.image}
+        alt={table?.name}
+        className="w-full sm:w-32 h-40 sm:h-32 rounded-lg object-cover"
+      />
 
-            <div className="flex-1 min-w-0 mr-10">
-                {" "}
-                {/* Added right margin to prevent overlap */}
-                <span className="block truncate font-medium">{table?.name}</span>
-                <div className="mt-2 flex items-center gap-2 bg-[#037755] rounded-2xl px-2 py-1 w-fit text-sm">
-                    <button className="bg-[#B38C50] rounded-full p-1 flex-shrink-0">
-                        <Box size={18} />
-                    </button>
-                    <span>
-                        L:{table?.length} CM W:{table?.width} CM H:{table?.height}
-                    </span>
-                </div>
-                {/* Rate and Seats Section */}
-                <div className="flex justify-between mt-3">
-                    <div className="flex flex-col justify-center items-center">
-                        <span className="text-[#B38C50] text-lg">Rate</span>
-                        <div className="bg-[#037755] rounded-2xl px-3 py-1 mt-1 w-fit">{table?.rate} AED</div>
-                    </div>
+      {/* Content */}
+      <div className="flex-1 min-w-0 relative">
+        {/* Table name */}
+        <span className="block font-medium truncate text-lg">{table?.name}</span>
 
-                    <div className="flex flex-col justify-center items-center mr-4 ">
-                        <span className="text-[#B38C50] text-lg">Seats</span>
-                        <div className="bg-[#037755] rounded-2xl px-3 py-1 mt-1 w-fit">{table?.chairCount}</div>
-                    </div>
-                </div>
-            </div>
-
-            {/* Plus Button - Adjusted positioning */}
-            {buttonVisibility&&<>
-            {tables?.some((obj: any) => obj?._id == table?._id) ? (
-                <div
-                    onClick={() => dispatch(removeTables(table?._id))}
-                    className="absolute bottom-0  right-0 bg-[#B38C50] text-white p-5 rounded-full cursor-pointer"
-                >
-                    <Trash color="white" size={20} />
-                </div>
-            ) : (
-                <div onClick={() => dispatch(setTables(table))} className=" absolute bottom-0 right-0 p-5 bg-white  rounded-full cursor-pointer">
-                    <Plus color="black" size={20} />
-                </div>
-            )}
-            </>}
+        {/* Dimensions */}
+        <div className="mt-2 flex items-center gap-2 bg-[#037755] rounded-2xl px-2 py-1 w-fit text-sm">
+          <button className="bg-[#B38C50] rounded-full p-1 flex-shrink-0">
+            <Box size={18} />
+          </button>
+          <span>
+            L:{table?.length} CM W:{table?.width} CM H:{table?.height}
+          </span>
         </div>
-    );
+
+        {/* Rate and Seats */}
+        <div className="flex justify-between mt-3 gap-4">
+          <div className="flex flex-col items-center">
+            <span className="text-[#B38C50] text-sm sm:text-lg">Rate</span>
+            <div className="bg-[#037755] rounded-2xl px-3 py-1 mt-1 text-sm">{table?.rate} AED</div>
+          </div>
+
+          <div className="flex flex-col items-center">
+            <span className="text-[#B38C50] text-sm sm:text-lg">Seats</span>
+            <div className="bg-[#037755] rounded-2xl px-3 py-1 mt-1 text-sm">{table?.chairCount}</div>
+          </div>
+        </div>
+
+        {/* Add/Remove Button */}
+        {buttonVisibility && (
+          <>
+            {isSelected ? (
+              <div
+                onClick={() => dispatch(removeTables(table?._id))}
+                className="absolute bottom-0 right-0 bg-[#B38C50] text-white p-3 sm:p-5 rounded-full cursor-pointer"
+              >
+                <Trash size={20} />
+              </div>
+            ) : (
+              <div
+                onClick={() => dispatch(setTables(table))}
+                className="absolute bottom-0 right-0 bg-white text-black p-3 sm:p-5 rounded-full cursor-pointer"
+              >
+                <Plus size={20} />
+              </div>
+            )}
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
